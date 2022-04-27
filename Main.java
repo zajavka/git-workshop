3c679a04c8e4bed9ad81ab42998f34cdf99d74dd
zajavka <kontakt@zajavka.pl>
2022-04-27 19:26:46 +0200

    changes-3

--- Main.java
+++ Main.java
@@ -3,8 +3,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello!");
         System.out.println("Hello 2!");
-        System.out.println("Hello 3!");
-        System.out.println("Hello 3!");
-        System.out.println("Hello 3!");
     }
 }
